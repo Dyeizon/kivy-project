8e1927065bbd57e5c16451068215c2c4a9a37852
Dyeizon <dyeizonque@gmail.com>
2022-03-01 14:42:04 -0300

    initial main file

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import kivy
+
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.widget import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Test')
+
+
+if __name__ == '__main__':
+    KivyApp().run()
