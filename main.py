20f8d19f0542ba49043a6e4629b8756d006e04a2
Dyeizon <dyeizonque@gmail.com>
2022-03-01 17:48:34 -0300

    changes to test on android builder

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.lang.builder import Builder
-from kivy.uix.widget import Widget
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
 
-login_screen = Builder.load_file('login_screen_kv.kv')
+# from kivy.lang.builder import Builder
+
+import random
+
+kivy.require('2.0.0')
 
 
-class KivyApp(App):
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
+
+
+class TravelApp(App):
     def build(self):
-        return login_screen
+        return MyRoot()
 
 
 if __name__ == '__main__':
-    KivyApp().run()
+    TravelApp().run()
